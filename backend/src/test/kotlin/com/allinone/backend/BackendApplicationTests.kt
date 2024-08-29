44e7900f6c6f4aac338447ccb3031d89ae0dea5a
jonghun <jonghun00.park@samsung.com>
2024-08-30 08:49:47 +0900

    initial commit for package structure

---
+++ backend/src/test/kotlin/com/allinone/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.allinone.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
